9def8b9d33f4eb8949ccc44de618ff08ec782606
Telmo Silva <telmo.anastacio@gmail.com>
2020-08-28 09:58:31 +0200

    init

---
+++ src/main/java/com/tsilva/persistenceTask/PersistenceTaskApplication.java
@@ -0,0 +1,13 @@
+package com.tsilva.persistenceTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistenceTaskApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(PersistenceTaskApplication.class, args);
+	}
+}
---
+++ src/test/java/com/tsilva/persistenceTask/PersistenceTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsilva.persistenceTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistenceTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
